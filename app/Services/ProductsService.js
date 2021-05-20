6aa3dec04eb2752726b70e44d1f902dfa7bbb812
Brian Lipsett <lipsettbrian3@gmail.com>
2021-05-20 17:02:10 -0600

    Fixed Cart solution

--- app/Services/ProductsService.js
+++ app/Services/ProductsService.js
@@ -5,7 +5,10 @@ import { Product } from "../Models/Product.js";
 class ProductsService {
   constructor() {}
   addProduct(newItem) {
-    ProxyState.cart = [...ProxyState.cart, new Product(newItem)];
+    console.log(newItem);
+    // ProxyState.cart = [...ProxyState.cart, new Product(newItem)];
+    // Above doesn't work bc this casts the product into a new item AGAIN
+    ProxyState.cart = [...ProxyState.cart, newItem];
     let cart = ProxyState.cart;
     console.log("your", cart);
   }
